d2c044dd5d9d96f99f220a86c222e7d91ca7a71e
kaarnis <karnarantaveeti@gmail.com>
2024-04-09 00:06:36 +0300

    Use feign client to reduce boilerplate

--- currency-conversion-service/src/main/java/com/kaarnis/microservices/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/kaarnis/microservices/currencyconversionservice/CurrencyConversion.java
@@ -6,23 +6,22 @@ public class CurrencyConversion {
     private Long id;
     private String from;
     private String to;
-    private BigDecimal conversionMultiple;
     private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
     private BigDecimal totalCalculatedAmount;
     private String environment;
 
     public CurrencyConversion() {
-
     }
 
-    public CurrencyConversion(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+    public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple,
             BigDecimal totalCalculatedAmount, String environment) {
         super();
         this.id = id;
         this.from = from;
         this.to = to;
-        this.conversionMultiple = conversionMultiple;
         this.quantity = quantity;
+        this.conversionMultiple = conversionMultiple;
         this.totalCalculatedAmount = totalCalculatedAmount;
         this.environment = environment;
     }
--- currency-conversion-service/src/main/java/com/kaarnis/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/kaarnis/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -12,6 +12,12 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+
+    private final CurrencyExchangeProxy currencyExchangeProxy;
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
@@ -20,8 +26,8 @@ public class CurrencyConversionController {
     ) {
 
         HashMap<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from",from);
-        uriVariables.put("to",to);
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
 
 
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
@@ -33,7 +39,24 @@ public class CurrencyConversionController {
                 from, to, quantity,
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment()+ " " + "rest template");
+                currencyConversion.getEnvironment() + " " + "rest template");
+
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
 
     }
 }
